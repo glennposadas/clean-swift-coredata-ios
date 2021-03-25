1b20307aca6f8bc8af0e10e1aad41a8ebd3d4aaa
Glenn Posadas <hello@glennvon.com>
2021-03-26 01:51:14 +0800

    add project code base.

---
+++ Contacts/Scenes/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Glenn Posadas on 3/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+}
+
